9d30a0d25c6e2cb010a40964acf9f307c28cde56
Gabriel Rocha <gabriel.gabrielroch@gmail.com>
2021-11-09 23:52:18 -0300

    implementação das funções de inserção

--- bdcomunication.py
+++ bdcomunication.py
@@ -1,3 +1,30 @@
+resultado = {
+    "id_request": "teste",
+	"timestamp": "",
+	"moagem de calcario": "",
+	"moagem de silica": "",
+	"temperatura de clinquerizacao": "",
+	"grau de homogeneizacao": "",
+	"tempo de clinquerizacao": "",
+	"1° Resfriamento": "",
+	"2° Resfriamento": "",
+	"fsc farinha alimentada": "",
+	"peso do litro": "",
+	"chama": "",
+	"forno": "",
+    "imgs": []
+    }
+img = {
+        "img_path": "teste01",
+	    "img_result": "string",
+	    "alita C3S": {
+		    "forma": "string",
+		    "grau de decomposicao": "string",
+		    "inclusao cristalina": "string",
+		    "diametro medio": "string"
+	    }
+    }
+
 def get_database():
     """Faz a conexão com o banco de dados e retorna o mesmo"""
     from pymongo import MongoClient
@@ -8,27 +35,31 @@ def get_database():
 
     return cliente["myFirstDatabase"]
 
-#def get_idRequest():
-#    """Busca o id na lista e retorna para a consulta"""
-#    id_request = "dfe0cd85-8e62-4287-96a5-3f5d1490d926"
-#
-#    return id_request
-#
-#def getAllanalises():
-#    """Retorna todos as analises registradas"""
-#    db = get_database() 
-#    collection = db["registros"]
-#    
-#    return collection.find()
-
-def getAnalise(id_request):
+def getAnalise(id_request: str):
     """
-        Retorna dados importantes da registro a ser analisado
-        e alterado o status para "Análise em andamento"
+        Recebe o id e Retorna dados importantes do registro a ser analisado
+        e altera o status para "Análise em andamento"
     """   
     db = get_database() 
     collection = db["registros"]
 
-    objeto = collection.find_one_and_update({'id_request': id_request}, {'$set': {'status': "Análise em andamento"}})
+    registro = collection.find_one_and_update({'id_request': id_request}, {'$set': {'status': "Análise em andamento"}})
 
-    return objeto['img_list'], objeto['id_request']
+    return registro['img_list'], registro['id_request']
+
+def writeResults(result: dict):
+    """
+        Recebe e Insere um dict no bd, o objetivo é incluir apenas o 
+        resultado, mas também adiciona o dict contendo as imagens.
+    """
+    db = get_database()
+    collection = db['resultados']
+
+    collection.insert_one(result)
+
+def writeImgsInResults(imgs: list, id_request: str):
+    """Recebe uma lista e um id, Insere essa lista no documento que vai ser buscado pelo id."""
+    db = get_database()
+    collection = db['resultados']
+
+    collection.find_one_and_update({'id_request': id_request}, {'$set': {'imgs': imgs}})
--- worker.py
+++ worker.py
@@ -9,15 +9,6 @@ def startWorker():
     socket = context.socket(zmq.PULL)
     socket.connect("tcp://127.0.0.1:3001")
 
-def readList():
-    """Lê a lista e retorna o último id adicionado, necessário alteração para LIFO"""
-    id_request = listIds.pop()
-    return id_request
-
-def writeList():
-    """Escreve em um arquivo de texto o id"""
-    print("")
-
 def formatId(id_request):
     """Formata o id, deixando ele pronto para ser usado na consulta do bd"""
     id_request = str(id_request).replace("'", "")
@@ -25,20 +16,15 @@ def formatId(id_request):
     return id_request
 
 def main():
+    print("\n Leitor iniciado na porta 3001 \n")
     startWorker()
-    global listIds
-
-    listIds = []
 
     while True:
         id_request = socket.recv()
         if id_request:
-            
-
-            #função que inicia o algoritmo passando o id_requesr por parâmetro
-            #Essa função pode retornar o json que vai ser cadastrado no bd para logo depois o algoritmo que lida com o bd cadastrar
             list_img, id_req = getAnalise(formatId(id_request))
-            print("\n" + id_req + "\n")
+            #Colocar a função que inicia o algoritmo
+            print("\nid request = \n" + id_req)
             for x in list_img:
                 print(x)
 
